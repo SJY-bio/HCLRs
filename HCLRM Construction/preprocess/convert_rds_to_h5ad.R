5c04f94085987e7d7ccfb6d1815614314b73b943
SJY-bio <shijingyi@hrbmu.edu.cn>
2026-01-29 01:20:37 +0800

    Refactor R script for converting RDS to H5AD
    Updated file paths to be relative and removed unnecessary whitespace.

--- HCLRM Construction/preprocess/convert_rds_to_h5ad.R
+++ HCLRM Construction/preprocess/convert_rds_to_h5ad.R
@@ -1,5 +1,3 @@
-##################### plan A #####################
-setwd("D:/ATAC/HCLRs/GSE176078/")
 library(Seurat)
 library(qs)
 library(reticulate)
@@ -10,13 +8,13 @@ library(data.table)
 library(tidyr)
 loompy <- reticulate::import('loompy')
 # register(MulticoreParam(workers = 4, progressbar = TRUE)) 
-IPF <- dir("D:/ATAC/HCLRs/GSE176078/sc_rds/")
+IPF <- dir("GSE176078/sc_rds/")
 IPF <- sub("\\.rds$", "", IPF)
 sp = 1
 
 for (sp in 1:length(IPF)) {
   # Seurat to AnnData
-  IPF_seurat = readRDS(paste0("sc_rds/",dir("D:/ATAC/HCLRs/GSE176078/sc_rds/")[sp]))
+  IPF_seurat = readRDS(paste0("sc_rds/",dir("GSE176078/sc_rds/")[sp]))
   IPF_seurat <- JoinLayers(IPF_seurat)
   IPF_seurat <- FindVariableFeatures(IPF_seurat,verbose = F) %>%
     NormalizeData(verbose = F) %>%
@@ -25,14 +23,15 @@ for (sp in 1:length(IPF)) {
     RunUMAP(dims = 1:30, verbose = FALSE)
   IPF_seurat[["RNA"]] <- as( IPF_seurat[["RNA"]], "Assay")
   sceasy::convertFormat( IPF_seurat, from="seurat", to="anndata", assay = "RNA", main_layer = "count",
-                         outFile=paste0("D:/ATAC/HCLRs/GSE176078/sc_h5ad/",IPF[sp],".h5ad"))
+                         outFile=paste0("GSE176078/sc_h5ad/",IPF[sp],".h5ad"))
   meta <- IPF_seurat@meta.data
   df_meta <- tibble(
     Cell      = rownames(meta),
     cell_type = meta$cell.type
   )
-  fwrite(df_meta,file = paste0("D:/ATAC/HCLRs/GSE176078/sc_h5ad/",IPF[sp],"_meta.txt"),quote = F,sep = "\t")
+  fwrite(df_meta,file = paste0("GSE176078/sc_h5ad/",IPF[sp],"_meta.txt"),quote = F,sep = "\t")
 }
+
 
 
 
